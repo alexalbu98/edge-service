0a962025fc9add63ae1f167fe112b9edab4b5c13
Alex Albu <alexalbu78@gmail.com>
2024-11-28 10:56:59 +0200

    Added rate limiter for each user.
    Signed-off-by: Alex Albu <alexalbu78@gmail.com>

--- src/main/java/com/polarbookshop/edge_service/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/RateLimiterConfig.java
@@ -3,12 +3,15 @@ package com.polarbookshop.edge_service.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Configuration
 public class RateLimiterConfig {
     @Bean
-    public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
+    KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
